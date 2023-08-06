a62228eeb5e872b01dba4c61594339d8b022058b
Nithin-Varma <mnithin1422@gmail.com>
2023-08-06 20:08:05 +0530

    abi kaam kar rha hai

--- src/server.ts
+++ src/server.ts
@@ -1,103 +1,42 @@
-// import express,{NextFunction, Request,Response} from 'express';
-// import { getSharedIdempotencyService, idempotency } from 'express-idempotency';
-// import axios from 'axios';
-// import cors from 'cors'
-// import env from "dotenv";
-// import bodyparser from "body-parser"
-// env.config()
-
-// const cache = new Map();
-
-// const app = express();
-// app.use(bodyparser.json());
-// app.use(bodyparser.urlencoded({ extended: true }));
-// app.use(idempotency());
-// app.use(cors())
-
-// const PORT = process.env.PORT || 30002;
-
-// app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
-//     const startTime = Date.now();
-//     res.on('finish', () => {
-//       const endTime = Date.now();
-//       const requestTime = endTime - startTime;
-//       console.log(`Request fulfilled in ${requestTime}ms`);
-//     });
-//     const body = req.body
-//     const idempotencyService = getSharedIdempotencyService();
-
-//     if(idempotencyService.isHit(req)) {
-//       return;
-//     }
-
-//   try {
-//     const idempotencyKey = req.headers['idempotency-key'];
-
-//     const response = await axios.post(
-//       `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_KEY}`,
-
-//       body,
-//       {
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//       }
-//     );
-
-//     console.log('dhek, now we are fetching the data from the api, because we didnt have any already fetched data...');
-//     console.log(response.data)
-//     return res.send(response.data)
-//     next();
-//   } catch (error) {
-//     idempotencyService.reportError(req)
-//     return res.send(error)
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-import express,{Request,Response} from 'express';
-import { idempotency } from 'express-idempotency';
+import express,{NextFunction, Request,Response} from 'express';
+import { getSharedIdempotencyService, idempotency } from 'express-idempotency';
 import axios from 'axios';
 import cors from 'cors'
 import env from "dotenv";
+import loggers from "pino-http"
 env.config()
-
-const cache = new Map();
 
 const app = express();
 app.use(express.json());
-app.use(idempotency());
+app.use(loggers())
 app.use(cors())
+app.use(idempotency());
+
 
 const PORT = process.env.PORT || 30002;
 
-app.post('/sepolia', async (req: Request, res: Response) => {
-  try {
-    const idempotencyKey = req.headers['idempotency-key'];
+app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
     const startTime = Date.now();
     res.on('finish', () => {
       const endTime = Date.now();
       const requestTime = endTime - startTime;
       console.log(`Request fulfilled in ${requestTime}ms`);
     });
+    const body = req.body
+    const idempotencyService = getSharedIdempotencyService();
+    console.log(idempotencyService)
 
-    if (cache.has(idempotencyKey)) {
-      const cachedResponse = cache.get(idempotencyKey);
-      console.log('dhek, now we already have a fetched data... so now we are retrieving the existing one...');
-      res.send(cachedResponse);
+    if(idempotencyService.isHit(req)) {
+      console.log("Request matched idempotency key")
       return;
     }
+
+  try {
 
     const response = await axios.post(
       `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_KEY}`,
 
-      req.body,
+      body,
       {
         headers: {
           'Content-Type': 'application/json'
@@ -105,52 +44,58 @@ app.post('/sepolia', async (req: Request, res: Response) => {
       }
     );
 
-    cache.set(idempotencyKey, response.data);
-
     console.log('dhek, now we are fetching the data from the api, because we didnt have any already fetched data...');
-    res.send(response.data); 
+    console.log(response.data)
+    res.send(response.data)
+    next();
+
   } catch (error) {
-    console.log('An error occurred', error);
+    idempotencyService.reportError(req);
+    return res.status(500).send('Internal Server Error');
   }
 });
 
-app.post('/mumbai', async (req: Request, res: Response) => {
-  try {
-    const idempotencyKey = req.headers['idempotency-key'];
 
-    const startTime = Date.now();
-    res.on('finish', () => {
-      const endTime = Date.now();
-      const requestTime = endTime - startTime;
-      console.log(`Request fulfilled in ${requestTime}ms`);
-    });
+app.post('/mumbai', async (req: Request, res: Response, next:NextFunction) => {
+  const startTime = Date.now();
+  res.on('finish', () => {
+    const endTime = Date.now();
+    const requestTime = endTime - startTime;
+    console.log(`Request fulfilled in ${requestTime}ms`);
+  });
+  const body = req.body
+  const idempotencyService = getSharedIdempotencyService();
+  console.log(idempotencyService)
 
-    if (cache.has(idempotencyKey)) {
-      const cachedResponse = cache.get(idempotencyKey);
-      console.log('dhek, now we already have a fetched data... so now we are retrieving the existing one...');
-      res.send(cachedResponse);
-      return;
+  if(idempotencyService.isHit(req)) {
+    console.log("Request matched idempotency key")
+    return;
+  }
+
+try {
+
+  const response = await axios.post(
+    `https://eth-sepolia.g.alchemy.com/v2/${process.env.MUMBAI_KEY}`,
+
+    body,
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      },
     }
+  );
 
-    const response = await axios.post(
-      `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_KEY}`,
+  console.log('dhek, now we are fetching the data from the api, because we didnt have any already fetched data...');
+  console.log(response.data)
+  res.send(response.data)
+  next();
 
-      req.body,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-    );
-
-    cache.set(idempotencyKey, response.data);
-
-    console.log('dhek, now we are fetching the data from the api, because we didnt have any already fetched data...');
-    res.send(response.data);
-  } catch (error) {
-    console.log('An error occurred', error);
-  }
+} catch (error) {
+  idempotencyService.reportError(req);
+  return res.status(500).send('Internal Server Error');
+}
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
