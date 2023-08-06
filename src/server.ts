b04adae98825de46081a2910033a12f9d789a4ee
Nithin-Varma <mnithin1422@gmail.com>
2023-08-06 19:07:43 +0530

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,66 @@
+// import express,{NextFunction, Request,Response} from 'express';
+// import { getSharedIdempotencyService, idempotency } from 'express-idempotency';
+// import axios from 'axios';
+// import cors from 'cors'
+// import env from "dotenv";
+// import bodyparser from "body-parser"
+// env.config()
+
+// const cache = new Map();
+
+// const app = express();
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: true }));
+// app.use(idempotency());
+// app.use(cors())
+
+// const PORT = process.env.PORT || 30002;
+
+// app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
+//     const startTime = Date.now();
+//     res.on('finish', () => {
+//       const endTime = Date.now();
+//       const requestTime = endTime - startTime;
+//       console.log(`Request fulfilled in ${requestTime}ms`);
+//     });
+//     const body = req.body
+//     const idempotencyService = getSharedIdempotencyService();
+
+//     if(idempotencyService.isHit(req)) {
+//       return;
+//     }
+
+//   try {
+//     const idempotencyKey = req.headers['idempotency-key'];
+
+//     const response = await axios.post(
+//       `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_KEY}`,
+
+//       body,
+//       {
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//       }
+//     );
+
+//     console.log('dhek, now we are fetching the data from the api, because we didnt have any already fetched data...');
+//     console.log(response.data)
+//     return res.send(response.data)
+//     next();
+//   } catch (error) {
+//     idempotencyService.reportError(req)
+//     return res.send(error)
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
 import express,{Request,Response} from 'express';
 import { idempotency } from 'express-idempotency';
 import axios from 'axios';
@@ -17,6 +80,12 @@ const PORT = process.env.PORT || 30002;
 app.post('/sepolia', async (req: Request, res: Response) => {
   try {
     const idempotencyKey = req.headers['idempotency-key'];
+    const startTime = Date.now();
+    res.on('finish', () => {
+      const endTime = Date.now();
+      const requestTime = endTime - startTime;
+      console.log(`Request fulfilled in ${requestTime}ms`);
+    });
 
     if (cache.has(idempotencyKey)) {
       const cachedResponse = cache.get(idempotencyKey);
@@ -48,6 +117,13 @@ app.post('/sepolia', async (req: Request, res: Response) => {
 app.post('/mumbai', async (req: Request, res: Response) => {
   try {
     const idempotencyKey = req.headers['idempotency-key'];
+
+    const startTime = Date.now();
+    res.on('finish', () => {
+      const endTime = Date.now();
+      const requestTime = endTime - startTime;
+      console.log(`Request fulfilled in ${requestTime}ms`);
+    });
 
     if (cache.has(idempotencyKey)) {
       const cachedResponse = cache.get(idempotencyKey);
