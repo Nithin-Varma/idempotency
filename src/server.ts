1cdf9147c61e09858d8c9d57aa328eaddc093103
Nithin-Varma <mnithin1422@gmail.com>
2023-08-09 15:37:31 +0530

    arey, finally idempotency solved...

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,17 @@ import cors from "cors";
 import env from "dotenv";
 import pino from 'pino'
 import expressPino from 'express-pino-logger'
+import QuickLRU from 'quick-lru';
+
+
 // import log from 'axios-debug-log'
 
-env.config();
-
-
-
-
+env.config(); 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
-app.use(idempotency());
+// app.use(idempotency());
 
 
 const PORT = process.env.PORT || 30002;
@@ -31,6 +30,11 @@ app.use(logRequest)
 // const logger = pino({})
 // const alchemyLogger = expressPino({logger})
 // app.use(alchemyLogger)
+
+const cache = new QuickLRU({
+  maxSize:100000000,
+})
+
 
 
 axios.interceptors.request.use(function (config) {
@@ -61,6 +65,40 @@ axios.interceptors.response.use(function (response) {
   
   return Promise.reject(error);
 });
+interface PendingRequests {[key:string]: Response<any, Record<string, any>>[]}
+const pendingRequests:PendingRequests = {}
+
+
+// app.use((req: Request, res:Response, next:NextFunction) => {
+//   const key = req.headers['idempotency'] as string;
+//   if(key && pendingRequests[key]) {
+//     pendingRequests[key].push(res);
+//     return
+//   }
+
+//   if(key) {
+//     pendingRequests[key] = [res]
+//   }
+//   next()
+// })
+
+app.use((req:Request, res:Response, next:NextFunction) => {
+  const key = req.headers['Idempotency-Key']
+
+  if (key && cache.has(key)) {
+    return res.end(cache.get(key)) 
+  }
+  //@ts-ignore
+  res.originalSend = res.send
+  //@ts-ignore
+  res.send = (body) => {
+    cache.set(key, body)  
+    //@ts-ignore
+    res.originalSend(body)
+  }
+
+  next()
+})
 
 
 app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
@@ -70,14 +108,33 @@ app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
       const requestTime = endTime - startTime;
       console.log(`Request fulfilled in ${requestTime}ms`);
     });
-    const body = req.body;
-    const idempotencyService = getSharedIdempotencyService();
-    console.log(idempotencyService);
 
-    if (idempotencyService.isHit(req)) {
-      console.log("Request matched idempotency key");
-      return;
-    }
+    // const key = req.headers['idempotency'] as string;
+
+  //   if(key) {
+  //     const cacheResponse = cache.get(key)
+
+  //     if(cacheResponse) {
+  //       pendingRequests[key].forEach(r => {
+  //         r.send(cacheResponse)
+  //       })
+  //       delete pendingRequests[key];
+  //       return res.send(cacheResponse)
+  //     }
+  // }
+    const body = req.body;
+
+
+
+    // const idempotencyService = getSharedIdempotencyService();
+    // console.log(idempotencyService);
+
+
+
+    // if (idempotencyService.isHit(req)) {
+    //   console.log("Request matched idempotency key");
+    //   return;
+    // }
 
     try {
       const response = await axios.post(
@@ -91,16 +148,23 @@ app.post('/sepolia', async (req: Request, res: Response, next:NextFunction) => {
         }
       );
 
+      // if(key) {
+      //   //ts
+      //   cache.set(key, response)
+      // }
+      // res.send(response.data)
+
       console.log(
         "dhek, now we are fetching the data from the api, because we didnt have any already fetched data..."
       );
       console.log(response.data);
       res.send(response.data);
   
-      next();
+      
     } catch (error) {
-      idempotencyService.reportError(req);
-      return res.status(500).send("Internal Server Error");
+      // idempotencyService.reportError(req);
+      console.log({error})
+      res.status(500).send("Internal Server Error");
     }
   }
 );
@@ -114,13 +178,6 @@ app.post("/mumbai", async (req: Request, res: Response, next: NextFunction) => {
     console.log(`Request fulfilled in ${requestTime}ms`);
   });
   const body = req.body;
-  const idempotencyService = getSharedIdempotencyService();
-  console.log(idempotencyService);
-
-  if (idempotencyService.isHit(req)) {
-    console.log("Request matched idempotency key");
-    return;
-  }
 
   try {
     const response = await axios.post(
@@ -139,10 +196,8 @@ app.post("/mumbai", async (req: Request, res: Response, next: NextFunction) => {
     );
     console.log(response.data);
     res.send(response.data);
-    next();
   } catch (error) {
-    idempotencyService.reportError(req);
-    return res.status(500).send("Internal Server Error");
+    res.status(500).send("Internal Server Error");
   }
 });
 
